72298185dd8fc295c73946355eb7a803d7aaad18
Michal Gajda <mgajda36@gmail.com>
2026-03-10 13:47:29 +0100

    feat: added rel attribute for safety

--- src/components/Attribution.tsx
+++ src/components/Attribution.tsx
@@ -2,13 +2,18 @@ export default function Attribution() {
   return (
     <div className="attribution">
       Challenge by&nbsp;
-      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         Frontend Mentor
       </a>
       . Coded by&nbsp;
       <a
         href="https://github.com/MichalGajda/fm-qr-code-component"
         target="_blank"
+        rel="noopener noreferrer"
       >
         Michał Gajda
       </a>
